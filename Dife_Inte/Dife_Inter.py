e841b7e863ffa7d0333911ec498d282972030a5f
Luis Eduardo Bautista Gaona <EduardoBautista9916@users.noreply.github.com>
2020-05-14 13:07:19 -0500

    14052020_13:07_M

---
+++ Dife_Inte/Dife_Inter.py
@@ -0,0 +1,42 @@
+from recursos.barras import *
+import os
+
+
+puntos = []
+
+def diferenciacion(punto):
+    
+    pass
+
+def integracion():
+    pass
+
+def menuD_I():
+    os.system("cls")
+    barra(100)
+    sl(1)
+    barra(48)
+    print("MENÚ", end="")
+    barra(48)
+    sl(1)
+    barra(100)
+    sl(1)
+    barra(2)
+    espacios(2)
+    print("1.- Diferenciación", end="")
+    espacios(76)
+    barra(2)
+    sl(1)
+    barra(2)
+    espacios(2)
+    print("2.- Integración", end="")
+    espacios(79)
+    barra(2)
+    sl(1)
+    barra(100)
+    sl(1)
+    barra(100)
+    sl(1)
+    print("Elige una de las dos opciones")
+
+menuD_I()
--- interpolacion/gaussJordan.py
+++
@@ -1,2 +0,0 @@
-def gaussJordan(matriz):
-    sizeX=len(matriz)
